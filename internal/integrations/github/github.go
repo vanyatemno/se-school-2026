346ad2d1351bed0ddee65989ac4b073815c2f8b6
vanyatemno <vanya04400@gmail.com>
2026-04-11 05:24:00 +0200

    feat: github rate limit error handling

--- internal/integrations/github/github.go
+++ internal/integrations/github/github.go
@@ -2,8 +2,9 @@ package github
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"se-school/internal/config"
+	"time"
 
 	"github.com/google/go-github/v84/github"
 	"go.uber.org/zap"
@@ -21,15 +22,23 @@ func New(cfg *config.Github) *GithubService {
 }
 
 func (g *GithubService) GetRepositoryVersion(ctx context.Context, owner, repositoryName string) (string, error) {
-	release, res, err := g.client.Repositories.GetLatestRelease(ctx, owner, repositoryName)
+	release, _, err := g.client.Repositories.GetLatestRelease(ctx, owner, repositoryName)
 	if err != nil {
+		if rateLimitErr, ok := errors.AsType[*github.RateLimitError](err); ok {
+			zap.L().Warn(
+				"github integration just hit rate limit",
+				zap.Time("rate limit reset time", rateLimitErr.Rate.GetReset().Time),
+			)
+			waitDuration := time.Until(rateLimitErr.Rate.GetReset().Time)
+			select {
+			case <-time.After(waitDuration):
+				return g.GetRepositoryVersion(ctx, owner, repositoryName)
+			case <-ctx.Done():
+				return "", ctx.Err()
+			}
+		}
 		zap.L().Error("failed to get repository version", zap.Error(err))
 		return "", err
-	}
-	// todo: handle rate limit errors
-	if res.StatusCode != 200 {
-		zap.L().Error("failed to get repository version", zap.Int("status", res.StatusCode))
-		return "", fmt.Errorf("failed to get repository version, api status: %s", res.Status)
 	}
 
 	return release.GetTagName(), nil
