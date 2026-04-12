8cbe0cf122754115f83329ce77dfd7dcaf3a1386
vanyatemno <vanya04400@gmail.com>
2026-04-12 06:04:28 +0200

    feat: unsubscribe action

--- internal/services/subscription/unsubscribe.go
+++ internal/services/subscription/unsubscribe.go
@@ -1,7 +1,28 @@
 package subscription
 
-import "se-school/internal/models/dto"
+import (
+	"se-school/internal/models"
+	"se-school/internal/models/dto"
+
+	"go.uber.org/zap"
+)
 
 func (s *Service) Unsubscribe(req *dto.UnsubscribeRequest) error {
+	code, err := s.codesRepository.Get(req.Token)
+	if err != nil {
+		zap.L().Error("failed to find unsub code", zap.Error(err))
+		return err
+	}
+	subscription, err := s.subscriptionsRepository.GetByCode(code.ID, models.CodeTypeUnsubscribe)
+	if err != nil {
+		zap.L().Error("failed to find subscription", zap.Error(err))
+		return err
+	}
+	err = s.subscriptionsRepository.Delete(subscription.ID)
+	if err != nil {
+		zap.L().Error("failed to delete subscription", zap.Error(err))
+		return err
+	}
+
 	return nil
 }
